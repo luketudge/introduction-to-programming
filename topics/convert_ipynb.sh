ba90a6b3423fa45f1baf9df5c92b0ce4eb024e4b
luketudge <luketudge@gmail.com>
2019-11-12 09:05:09 +0100

    forget html

--- topics/convert_ipynb.sh
+++ topics/convert_ipynb.sh
@@ -6,9 +6,3 @@
 # Use these as the default pages linked from the README.
 jupyter nbconvert --to markdown *.ipynb
 sed -i "s/\.ipynb/.md/g" *.md
-
-# HTML.
-# Not really using these, but might as well make them.
-# Put them in a separate folder to avoid cluttering the page.
-jupyter nbconvert --to html *.ipynb --output-dir html
-sed -i "s/\.ipynb/.html/g" html/*.html
