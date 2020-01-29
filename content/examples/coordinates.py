72e13e71afd0dfff7b96c6cece81327e7815df73
luketudge <luketudge@gmail.com>
2020-01-29 15:17:34 +0100

    in-progress update

---
+++ content/examples/coordinates.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+A module for working with coordinates.
+Uses numpy arrays.
+"""
+
+import math
+
+import numpy
+
+
+def rotate(coords, angle):
+    """Rotates an array of x,y coordinates.
+
+    Arguments:
+        coords: numpy array of coordinates, with n rows and 2 columns (x,y).
+        angle: Angle of rotation, in degrees.
+
+    Returns: numpy array of rotated coordinates.
+    """
+
+    theta = math.radians(angle)
+
+    rotation_matrix = numpy.array([[math.cos(theta), math.sin(theta)],
+                                   [-math.sin(theta), math.cos(theta)]])
+
+    return numpy.matmul(coords, rotation_matrix)
