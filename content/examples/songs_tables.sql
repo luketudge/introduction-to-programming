ebc80e05a5fbd359594b6da5c5e4f13f33708970
luketudge <luketudge@gmail.com>
2020-03-04 15:18:06 +0100

    in-progress update

--- content/examples/songs_tables.sql
+++ content/examples/songs_tables.sql
@@ -1,10 +1,15 @@
 -- SQL commands for creating the tables in the 'songs.db' database.
 
+-- artists:
+-- no duplicate artist names
 CREATE TABLE artists
 (id INTEGER PRIMARY KEY,
 artist TEXT UNIQUE,
 country TEXT);
 
+-- albums:
+-- no duplicate album names for the same artist
+-- album artist must exist in the artists table
 CREATE TABLE albums
 (id INTEGER PRIMARY KEY,
 album TEXT,
@@ -13,10 +18,15 @@ artist_id INTEGER,
 UNIQUE(album, artist_id),
 FOREIGN KEY(artist_id) REFERENCES artists(id));
 
+-- songs:
+-- no duplicate song names for the same album
+-- no duplicate song numbers for the same album
+-- song album must exist in the albums table
 CREATE TABLE songs
 (id INTEGER PRIMARY KEY,
 song TEXT,
 number INTEGER,
 album_id INTEGER,
+UNIQUE(song, album_id),
 UNIQUE(number, album_id),
 FOREIGN KEY(album_id) REFERENCES albums(id));
